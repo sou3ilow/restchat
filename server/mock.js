7de406773c4747470873da96e4329af2c26de0e6
Junichi Hashimoto <sou3ilow@gmail.com>
2020-05-25 09:02:46 +0900

    add index page

--- server/mock.js
+++ server/mock.js
@@ -1,17 +1,26 @@
+'use strict'
 
+//https://utano.jp/entry/2019/05/aws-serverless-express/
+const is_prod = !!process.env.AWS_REGION;
 const port = 8888;
-const base_path = "/v1"
+
+// https://zu2febudd4.execute-api.us-west-1.amazonaws.com/prod/
+
+const base_path = is_prod ? "" : "/v1";
 
 const cors_allow_origin = '*'
-const cors_allow_method = 'POST'
+const cors_allow_method = 'OPTIONS,POST'
 
-require('console-info');
-require('console-warn');
-require('console-error');
+if ( !is_prod ) {
+	require('console-info');
+	require('console-warn');
+	require('console-error');
+}
 
 /* DB wrap */
 
 process.env.AWS_PROFILE='restchat' 
+const compression = require('compression');
 const aws = require('aws-sdk');
 aws.config.update({region: 'us-west-1'});
 const table_name = "messages";
@@ -58,8 +67,7 @@ DB.prototype.query = function(timestamp, except, onscan) {
 
 	this.db.scan(params, (err, data)=> {
 		let results = [];
-
-		if (err) {
+if (err) {
 			console.error("[scan] error", err);
 		} else {
 			console.log("timestamp =", timestamp);
@@ -80,20 +88,59 @@ DB.prototype.query = function(timestamp, except, onscan) {
 let db = new DB();
 
 /* front */
-const express = require("express");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+//const compression = require('compression')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+const router = express.Router()
 const app = express();
 
-app.use(express.json());
+//router.use(cors())
+router.use(bodyParser.json());
+
+if ( is_prod ) {
+	console.log("in prod mode");
+	router.use(awsServerlessExpressMiddleware.eventContext());
+}
 
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', cors_allow_origin);
     res.append('Access-Control-Allow-Methods', cors_allow_method);
     res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.append('Access-Control-Max-Age', 864000);
     next();
 });
 
+
+exports.handler = function(ev, con) {
+	let reponse = {
+		satautCode: 200,
+		handlers: {
+			"Access-Control-Allow-Origin": "*"
+		},
+		body: JSON.strigify(ev)
+	}
+	con.succeed(response);
+}
+
+router.use(base_path + '/open', compression())
+
+// test
+router.get(base_path + '/', (req, res) => {
+  res.send("base route");
+});
+router.get('/', (req, res) => {
+  res.send("route");
+});
+
+router.get(base_path + '/open', (req, res) => {
+	res.sendFile(`${__dirname}/chat.html`)
+});
+
 /* quey */
-app.post(base_path + "/query", function(req, res) {
+router.post(base_path + "/query", function(req, res) {
 	console.info("query is called");
 	let body = req.body;
 
@@ -114,7 +161,7 @@ app.post(base_path + "/query", function(req, res) {
 });
 
 /* post */
-app.post(base_path + '/post', function(req, res) {
+router.post(base_path + '/post', function(req, res) {
 	console.info("post is called");
 	let body = req.body;
 
@@ -142,8 +189,13 @@ app.post(base_path + '/post', function(req, res) {
 	res.status(200).send("");
 });
 
-app.listen(port, function() {
+function on_listen() {
 	console.info("listen start at port=", port);
 	console.info("base path is", base_path);
-});
+}
+
+app.use('/', router);
+
+module.exports = app
+
 
